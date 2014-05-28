e19d294e10e82c44de17a43af9d2d900ac15ad11
leonixyz <leonixyz@gmail.com>
2014-05-28 10:40:02 +0200

    iniziato gui

---
+++ DistributedSystem/ejbModule/client/Client.java
@@ -0,0 +1,11 @@
+package client;
+
+import gui.Window;
+
+public class Client {
+
+	public static void main(String[] args) {
+		new Window();
+	}
+
+}
--- DistributedSystem/ejbModule/client/Flight.java
+++ DistributedSystem/ejbModule/client/Flight.java
@@ -3,12 +3,12 @@ package client;
 public class Flight {
 
 	private String departure, arrival, date;
-
+	// departure/arrival date, departure/arrival airport, airline, flight no.
 	public Flight (String departure, String arrival, String date) {
 
 		this.departure = departure;
 		this.arrival = arrival;
-		this.date = date;		
+		this.date = date;
 	}
 
 	public String toString() {
